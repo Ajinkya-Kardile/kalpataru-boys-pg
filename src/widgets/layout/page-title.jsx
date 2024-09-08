58483e5e877b5845fb49a0d73f5598be6d1a20cd
Ajinkya-Kardile <kardileajinkya@gmail.com>
2024-09-08 14:01:03 +0530

    page title modified

--- src/widgets/layout/page-title.jsx
+++ src/widgets/layout/page-title.jsx
@@ -4,8 +4,9 @@ import { Typography } from "@material-tailwind/react";
 export function PageTitle({ section, heading, children }) {
   return (
     <div className="mx-auto w-full px-4 text-center lg:w-6/12">
-      <Typography variant="lead" className="font-semibold" color="blue">{section}</Typography>
-      <Typography variant="h2" color="blue-gray" className="my-3">
+      <Typography variant="lead" className="font-semibold uppercase" color="blue">{section}</Typography>
+      <div className="w-20 h-[1px] mx-auto my-1 bg-red-500 rounded"></div>
+      <Typography variant="h2" color="blue-gray" className="my-2">
         {heading}
       </Typography>
       <Typography variant="lead" className="text-blue-gray-500">
